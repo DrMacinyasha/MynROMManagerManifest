1a7f1696ef83aecfba5e8cb683fbbda1a716f229
DrMacinyasha <drmacinyasha@gmail.com>
2011-04-07 15:31:57 -0700

    Add gh-pages

---
+++ manifest.js
@@ -0,0 +1,350 @@
+{
+    "version": "1",
+    "homepage": "http://forum.xda-developers.com/showthread.php?t=793471",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?business=myn_2000@yahoo.com&cmd=_xclick&currency_code=USD&item_name=Android Roms",
+    "mirrors": 
+    [
+    "http://sdx-downloads.com/EVO/devs/myn/",
+    "http://myn.goo-inside.me/",
+    "http://myn.ziggy471.com/",
+    "http://myn.onicrom.com/",
+    "http://chiznillen.net/myn/"
+    ],
+    "roms": 
+    [
+        {
+        "name": "Warm TwoPointTwo RLS 5",
+        "summary": "More Than Meets The Eye! - NOTE: PLEASE UPDATE MANUALLY UNLESS YOU ALREADY HAVE THE FOLLOWING: RADIO (2.15.00.11.19), WIMAX (27167_R01), PRI & NV (1.90_003) - http://tinyurl.com/WarmTwoPointTwo",
+        "device": "supersonic",
+        "product": "WarmTwoPointTwo",
+        "incremental": "5",
+        "modversion": "myn_Warm_TwoPointTwo_RLS5-Supersonic",
+        "screenshots": 
+        [
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/WarmTwoPointTwoDisplay.jpg",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/HomeScreen1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/Launcher.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/Dialer.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/RosieSettings1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/FriendFeed.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Future/LauncherPro-Option1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS4/Hulu.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Future/GingerBreadMenu/GingerBreadMenu.png"
+        ],
+        "file": "RomManager/WarmTwoPointTwo/RLS5/myn-WarmTwoPointTwo-01-07-2011-RLS-5.zip",
+        "addons": 
+        [
+        {
+            "name": "EVO Radio: 2.15.00.11.19 - WiMAX: 27167_R01 - PRI/NV: 1.90_003",
+            "file": "RomManager/WarmTwoPointTwo/Radios/EVO_Radio_2.15.00.11.19_WiMAX_27167_R01_PRI_NV_1.90_003.zip"
+        },
+        {
+            "name": "Transparent Titlebar",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-TransparentTitlebar-RLS5.zip"
+        },
+        {
+            "name": "Themed LauncherPro 0.8.2",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-LauncherPro-0.8.2-Warm2.2-Themed.zip"
+        },
+        {
+            "name": "Hulu (Do not update Flash from market)",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-Hulu-WarmTwoPointTwo-10-1-105-7.zip"
+        },
+        {
+            "name": "Car Dock Increased Volume Mod",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-CarDock-Volume-Increase.zip"
+        },
+        {
+            "name": "Data Throttling Removal Mod",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-Data-Governor-Removal-RLS5.zip"
+        },
+        {
+            "name": "Themed HTC_IME_MOD (From RLS3)",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-HTC-IME-MOD-WarmThemed.zip"
+        },
+        {
+            "name": "Browser With Reflow Enabled",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-Browser-Reflow-Enabled.zip"
+        },
+        {
+            "name": "GingerBread Keyboard",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-Gingerbread-Keyboard.zip"
+        },
+        {
+            "name": "BootAnimation From RLS3",
+            "file": "RomManager/WarmTwoPointTwo/RLS5/Updates/update-RLS3-BootAnimation.zip"
+        },
+        {
+            "name": "Collin_ph Battery Tweak For Evo",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update_Collin_Ph_Battery_Tweak-EVO.zip"
+        },
+        {
+            "name": "Stock Android Font",
+            "file": "RomManager/WarmTwoPointTwo/RLS1/Updates/update_StockFont-signed.zip"
+        },
+        {
+            "name": "DarkStar/Coconut Font",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-DarkStar-Coconut-Font.zip"
+        },
+        {
+            "name": "Gomorrahs WarmDesire FlipClock",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-gomorrah-WarmDesire-FlipClock.zip"
+        },
+        {
+            "name": "wrxmemp's WarmLines BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-WarmLines-BootAnimation.zip"
+        },
+        {
+            "name": "wrx4memp's Warm 3 BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-Warm3-BootAnimation.zip"
+        },
+        {
+            "name": "luniz7's BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-luniz7-BootAnimation.zip"
+        },
+        {
+            "name": "SBC for Netarchy's CFS More Aggressive 4.1.9.1 Kernel (v7)",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/SBC-net-4.1.9.1-more-havs-v7.zip"
+        },
+        {
+            "name": "Ziggy471 Warm TwoPointTwo RLS4 Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/update-Ziggy471Kernel-WarmTwoPointTwo-RLS4.zip"
+        },
+        {
+            "name": "KiNgxKernel #9 BFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-9-BFS318-2.6.32.15-Flashable.zip"
+        },
+        {
+            "name": "KiNgxKernel #11 CFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-11-2.6.32.24-Flashable.zip"
+        }
+        ]
+    },
+    {
+        "name": "Warm TwoPointTwo RLS 4",
+        "summary": "Making Sense Of It All!",
+        "device": "supersonic",
+        "product": "WarmTwoPointTwo",
+        "incremental": "4",
+        "modversion": "myn_Warm_TwoPointTwo_RLS4-Supersonic",
+        "screenshots": 
+        [
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/WarmTwoPointTwoDisplay.jpg",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/HomeScreen1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/Launcher.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/Dialer.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/RosieSettings1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/FriendFeed.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Future/LauncherPro-Option1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS4/Hulu.png"
+        ],
+        "file": "RomManager/WarmTwoPointTwo/RLS4/myn-WarmTwoPointTwo-11-25-2010-RLS-4.zip",
+        "addons": 
+        [
+        {
+            "name": "Transparent Titlebar",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-TransparentTitlebar-RLS4.zip"
+        },
+        {
+            "name": "Themed LauncherPro 0.8.1.1",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-LauncherPro-0.8.1.1-Warm2.2-Themed.zip"
+        },
+        {
+            "name": "Hulu (Do not update Flash from market)",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-Hulu-WarmTwoPointTwo-10-1-105-6.zip"
+        },
+        {
+            "name": "Themed HTC_IME_MOD (From RLS3)",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-HTC-IME-MOD-WarmThemed.zip"
+        },
+        {
+            "name": "Collin_ph Battery Tweak For Evo",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update_Collin_Ph_Battery_Tweak-EVO.zip",
+        },
+        {
+            "name": "Stock Android Font",
+            "file": "RomManager/WarmTwoPointTwo/RLS1/Updates/update_StockFont-signed.zip"
+        },
+        {
+            "name": "DarkStar/Coconut Font",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-DarkStar-Coconut-Font.zip"
+        },
+        {
+            "name": "Gomorrahs WarmDesire FlipClock",
+            "file": "RomManager/WarmTwoPointTwo/RLS4/Updates/update-gomorrah-WarmDesire-FlipClock.zip"
+        },
+        {
+            "name": "wrxmemp's WarmLines BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-WarmLines-BootAnimation.zip"
+        },
+        {
+            "name": "wrx4memp's Warm 3 BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-Warm3-BootAnimation.zip"
+        },
+        {
+            "name": "luniz7's BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-luniz7-BootAnimation.zip"
+        },
+        {
+            "name": "Ziggy471 Warm TwoPointTwo RLS4 Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/update-Ziggy471Kernel-WarmTwoPointTwo-RLS4.zip"
+        },
+        {
+            "name": "HTC Stock 2.6.32.15-g0f673ed #10 Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/update-htc-kernel-2.6.32.15-g0f673ed-10.zip"
+        },
+        {
+            "name": "KiNgxKernel #9 BFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-9-BFS318-2.6.32.15-Flashable.zip"
+        },
+        {
+            "name": "KiNgxKernel #11 CFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-11-2.6.32.24-Flashable.zip"
+        }
+        ]
+    },
+    {
+        "name": "Warm TwoPointTwo RLS 3",
+        "summary": "Have your cake and eat it too!",
+        "device": "supersonic",
+        "product": "WarmTwoPointTwo",
+        "incremental": "3",
+        "modversion": "myn_Warm_TwoPointTwo_RLS3-Supersonic",
+        "screenshots": 
+        [
+               "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/WarmTwoPointTwoDisplay.jpg",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/HomeScreen1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/Launcher.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/Dialer.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/RosieSettings1.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo-RLS3/FriendFeed.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Future/LauncherPro-Option1.png"
+        ],
+        "file": "RomManager/WarmTwoPointTwo/RLS3/myn-WarmTwoPointTwo-11-05-2010-RLS-3.zip",
+        "addons": 
+        [        
+        {
+            "name": "Transparent Titlebar",
+            "file": "RomManager/WarmTwoPointTwo/RLS3/Updates/update-TransparentTitlebar-RLS3.zip"
+        },
+        {
+            "name": "Themed LauncherPro 0.8.1.1",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-LauncherPro-0.8.1.1-Warm2.2-Themed.zip"
+        },        
+        {
+            "name": "Official EVO HTC_IME (With arrows)",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-EVO-HTC-IME.zip"
+        },
+        {
+            "name": "Collin_ph Battery Tweak For Evo",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update_Collin_Ph_Battery_Tweak-EVO.zip"
+        },
+        {
+            "name": "Stock Android Font",
+            "file": "RomManager/WarmTwoPointTwo/RLS1/Updates/update_StockFont-signed.zip"
+        },            
+        {
+            "name": "wrxmemp's WarmLines BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-WarmLines-BootAnimation.zip"
+        },    
+        {
+            "name": "wrx4memp's Warm 3 BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-Warm3-BootAnimation.zip"
+        },    
+        {
+            "name": "luniz7's BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-luniz7-BootAnimation.zip"
+        },        
+        {
+            "name": "KiNgxKernel #9 BFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-9-BFS318-2.6.32.15-Flashable.zip"
+        },
+        {
+            "name": "KiNgxKernel #11 CFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-11-2.6.32.24-Flashable.zip"
+        }
+        ]
+    },
+    {
+        "name": "Warm TwoPointTwo RLS 2",
+        "summary": "Have It Your Way!",
+        "device": "supersonic",
+        "product": "WarmTwoPointTwo",
+        "incremental": "2",
+        "modversion": "myn_Warm_TwoPointTwo_RLS2-Supersonic",
+        "screenshots": 
+        [
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/WarmTwoPointTwoDisplay.jpg",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/Launcher.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/FriendStream.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Future/LauncherPro-Option1.png"
+        ],
+        "file": "RomManager/WarmTwoPointTwo/RLS2/myn-WarmTwoPointTwo-10-15-2010-RLS-2.zip",
+        "addons": 
+        [
+        {
+            "name": "Transparent Titlebar",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-TransparentTitlebar.zip"
+        },
+        {
+            "name": "Themed LauncherPro 0.8.1.1",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-LauncherPro-0.8.1.1-Warm2.2-Themed.zip"
+        },
+        {
+            "name": "Official EVO HTC_IME (With arrows)",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-EVO-HTC-IME.zip"
+        },
+        {
+            "name": "Collin_ph Battery Tweak For Evo",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update_Collin_Ph_Battery_Tweak-EVO.zip"
+        },
+        {
+            "name": "Stock Android Font",
+            "file": "RomManager/WarmTwoPointTwo/RLS1/Updates/update_StockFont-signed.zip"
+        },
+        {
+            "name": "wrxmemp's WarmLines BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-WarmLines-BootAnimation.zip"
+        },
+        {
+            "name": "wrx4memp's Warm 3 BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-wrx4memp-Warm3-BootAnimation.zip"
+        },
+        {
+            "name": "luniz7's BootAnimation",
+            "file": "RomManager/WarmTwoPointTwo/RLS2/Updates/update-luniz7-BootAnimation.zip"
+        },
+        {
+            "name": "KiNgxKernel #9 BFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-9-BFS318-2.6.32.15-Flashable.zip"
+        },
+        {
+            "name": "KiNgxKernel #11 CFS Kernel",
+            "file": "RomManager/WarmTwoPointTwo/Kernels/KiNgxKernel-Evo-11-2.6.32.24-Flashable.zip"
+        }
+        ]
+    },
+    {
+        "name": "Warm TwoPointTwo RLS 1",
+        "summary": "An Enlightening Experience",
+        "device": "supersonic",
+        "product": "WarmTwoPointTwo",
+        "incremental": "1",
+        "modversion": "myn_Warm_TwoPointTwo_RLS1-Supersonic",
+        "screenshots": 
+        [
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/WarmTwoPointTwoDisplay.jpg",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/Launcher.png",
+            "http://i216.photobucket.com/albums/cc41/mynym/HTC%20EVO/ROMS/Warm%20TwoPointTwo%20-09-19-2010%20PreRls/FriendStream.png"
+        ],
+        "file": "RomManager/WarmTwoPointTwo/RLS1/myn-WarmTwoPointTwo-09-27-2010-RLS-1.zip",
+        "addons": 
+        [
+        {
+            "name": "Stock Android Font",
+            "file": "RomManager/WarmTwoPointTwo/RLS1/Updates/update_StockFont-signed.zip"
+        }
+        ]
+    }
+    ]
+}
